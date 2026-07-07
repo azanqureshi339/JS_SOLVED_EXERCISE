8ee1a49ee0b691aa333f811a6a330c2c4ada37da
Mohammad Azaan <azanqureshi339@gmail.com>
2026-07-07 19:08:51 +0500

    added chap:12-13 assignments

---
+++ CHAPTER-12-13/app.js
@@ -0,0 +1,104 @@
+//Question # 01
+
+//Question # 02
+
+
+// const num1 = +prompt("Entr a first number?");
+// const num2 = +prompt("Enter a second number?");
+// if (num1 == num2) {
+//     alert("Both are equal");
+// } else if (num1 > num2) {
+//     alert(num1 + " is greater.");
+// } else if (num2 > num1) {
+//     alert(num2 + " is greater");
+// } else {
+//     alert("Something went wrong...");
+// }
+
+//Question # 03
+
+// let letter = prompt("Enter a letter")
+
+
+
+// if (letter == "a") {
+//     console.log(true)
+// } else if (letter == "e") {
+//     console.log(true);
+// } else if (letter == "o") {
+//     console.log(true);
+// } else if (letter == "i") {
+//     console.log(true);
+// } else if (letter == "u") {
+//     console.log(true);
+// } else if (isNaN(letter) === false) {
+//     console.log("Number isn't allowed.");
+// } else {
+//     console.log(false);
+// }
+
+
+// == , ===
+// Double equal to value ko check krta hy sirf.
+// Triple equal to value , data type check krta hy
+// <=    --> 4 <= 4
+
+// >=          7 >=5
+
+
+//Question # 05
+
+// let myPassword = "12345"
+// const password = prompt("Enter your password!")
+// if(myPassword == password ){
+//     alert("Correct Password!")
+// }else{
+//     alert("Incorrect password!")
+// }
+
+//Question # 06
+
+// var greeting;
+// var hour = 13;
+// if (hour > 18){
+//     greeting = "Good day";
+// }else{
+//     greeting = "Good evening";
+// }
+
+//Question # 07
+// var time = prompt("24-hours") //15
+// var meridiem = undefined;
+// var timeIn12hours;
+// if(time > 24){
+//     alert("Invalid")
+// }
+// else if (time > 12){
+//     meridiem = "pm";
+//     timeIn12hours = time - 12;
+//     alert(timeIn12hours + meridiem) 
+// }
+// else{
+//     timeIn12hours = time;
+//     meridiem = "am"
+//     alert(timeIn12hours + meridiem)
+// }
+
+// Increment
+// Pre--- 
+// Post---
+
+// Decrement
+
+// let num = 5;
+// num--; // num = num - 1; // 4
+// num--; // num = num - 1;
+
+// console.log(num) // 3
+
+// String
+// Number
+// Boolean
+// undefined
+// Array
+
---
+++ chapter12/app.js
@@ -0,0 +1,71 @@
+// let userPrompt =prompt("kya azaan ZA aya hai ??")
+// if(userPrompt === "present"){
+//     alert("azaan aya hai ZA mein")
+// }else{
+//     alert("azaan nhi aya hai za")
+// }
+
+// let value = 20 ;
+// if(value > 10){
+//     console.log("value is greater then 10")
+// }else{
+//     console.log("value is  not greater then 10")
+
+// }
+
+
+// let value = 75;
+// if(value < 50 ){
+//     console.log("true")
+// }
+// else{console.log("false")}
+//     
+// 
+
+// let myAge = 50 
+// if(myAge >= 18){
+//     document.write("Aap vote kar sakte ho")
+// }else{
+// document.write("Aap vote nhi kar sakte")}
+// let userPercent = prompt("Enter your percentage", 70);
+
+
+// It's a positive number
+// It's a negative number
+// It's zero.
+// let userNumber = prompt("enter a number")
+// if (userNumber > 10) {
+
+// } else { alert("It's a possitive number") }
+
+// if (userNumber < 10) {
+
+// } else { alert("It's a negative number") }
+// if (userNumber = 0) {
+
+// } else { alert("It's a Zero") }
+
+// let userNumber = prompt("enter a number?")
+// if(userNumber == 0){
+//     alert("It's a Zero number")
+// }
+// else if (userNumber < 0){
+//     alert("It's a negative number")
+// }
+// else if (userNumber > 0){
+//     alert("It's a possitive number")
+// }
+// else{
+//     alert("Wrong Number...")
+// }
+// var num = prompt("Number Enter Karo");
+
+// if (num % 2 == 0) {
+//     alert("Even Number");
+// }
+// else if (num % 2 == 1) {
+//     alert("Odd Number");
+// }
+// else {
+//     alert("Wrong Number");
+// }
