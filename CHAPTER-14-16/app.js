e9ad1132a3d9623bf43474298efaabb3c56b3c20
Mohammad Azaan <azanqureshi339@gmail.com>
2026-07-07 19:11:07 +0500

    added chap:14-16 assignments

---
+++ CHAPTER-14-16/app.js
@@ -0,0 +1,71 @@
+// Nested array - multidimensional array
+// const cities = [];
+// cities.push("Abdul Moiz");
+// cities.push("Hannan");
+// cities.push("Mohammad Ali");
+
+// console.log(cities);
+
+// Question # 01 
+// const arr = ["Rehan" , "Azan" ,"Mohammad"];
+// console.log(arr[0]);
+
+// //Question # 02
+// const arr = new Array("Moiz" , "Ahmed");
+// console.log(arr);
+
+// Question # 03
+// const arr = ["Azan" , "Rehan" , "Mohammad"];
+// console.log(arr[0]);
+
+// Question # 04
+// const arr = [1 ,2 ,3];
+// console.log(arr[1]);
+
+// Question # 05
+// const arr = ["true" , "false"];
+// console.log(arr[0])
+
+// Question # 06
+// const arr = ["Azan" , 1 , 2 ,3 , true ];
+// console.log(arr[3]);
+
+// Question # 07
+// const qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PHD"];
+// document.write(qualifications[0] +"<br>");
+// document.write(qualifications[1] +"<br>");
+// document.write(qualifications[2] +"<br>");
+// document.write(qualifications[3] +"<br>");
+// document.write(qualifications[4] +"<br>");
+// document.write(qualifications[5] +"<br>");
+// document.write(qualifications[6] +"<br>");
+// document.write(qualifications[7] +"<br>");
+
+// const arr = [1, 2, 3, 4, 5, 6, "moiz", "ahmed"];
+// console.log(arr[arr.length - 1]);
+
+// Push=> last sy add krne k lye use hota hy
+// Pop=> last sy delete/remove krne k lye use hota hy
+// unshift=> start sy element ko add krta hy
+// shift=> start sy element ko remove krta hy
+
+// const arr = [1, 2, 3, 4];
+// // splice---> splice()
+
+
+// arr.splice(2, 0, "Moiz");
+// console.log(arr);
+// const students = ["Azan", "Rehan", "Mohammad"];
+// const scores = [480, 390, 400];
+
+// const totalMarks = 500;
+// const student1Percent = scores[0] / totalMarks * 100;
+// const student2Percent = scores[1] / totalMarks * 100;
+// const student3Percent = scores[2] / totalMarks * 100;
+
+// document.write("Score of " + students[0] + " is " + scores[0] + ". Percentage: " + student1Percent + "%");
+// document.write("<br>Score of " + students[1] + " is " + scores[1] + ". Percentage: " + student2Percent + "%");
+// document.write("<br>Score of " + students[2] + " is " + scores[2] + ". Percentage: " + student3Percent + "%");
+
+// const cities = ["karachi", "isl", "balochistan"];
+
