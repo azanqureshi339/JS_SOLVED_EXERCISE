aeae695e46ea11a2a3aab8de30f9e63970202537
Mohammad Azaan <azanqureshi339@gmail.com>
2026-07-07 19:04:47 +0500

    added: chap: 6-9 assignment

---
+++ CHAPTER-6-9/app.js
@@ -0,0 +1,80 @@
+
+
+// Redeclear & Re-assign kar sakte hain 
+// Redeclear-->mean=ek hii value ko 2 baar likh sakte hain...
+// Re-assign-->mean=value ko ubdate kar sakte hain...
+
+// var name = "Azan"
+// var name = "moiz"
+// name = "tayyab"
+// console.log(name)
+
+//Redeclear nhi kar sakte....mean-->ek hii value ko 2 baar nhi likh sakte. but Re-assign kar sakte hain...mean-->ubdate kar sakte hain 
+
+
+// let name1 = "Azan"
+// let name2 = "tayyab"
+// console.log(name1)
+
+// Redeclear & Re-assign nhi kar sakte-->na ubdate kar sakte hain na ek hi value ko 2 baar likh sakte hain.
+
+// String
+// Number
+// NaN
+// undefined *
+
+// declaration
+// js line-by line run karti hy.
+// let myName = undefined;
+
+// initialization
+// myName = "Moiz";
+
+// console.log("abc")
+// console.log(myName);
+// var myName = "Moiz";
+// var myName = "Moiz";
+// console.log(myName)
+
+// console
+// var, let and const
+
+// String---> ' ""
+// Number--> 89684396 , -54,  5.6
+
+
+
+
+// BODMAS ---> Bracket of divide multiply , addition, subtraction
+
+// Modulus
+// % *
+// let num = 5;
+// console.log(++num); //num = num + 1; // 5
+// console.log(num); // 6 
+
+//Questio # 01 //
+var a = 10;
+document.write("Result:<br>The value of ++a is:" + a + "");
+document.write("<br>.........................................")
+
+ a++ ;
+document.write(" <br><br>The value of a++ is " + a + "")
+document.write("<br> Not the value of a is " + a +"")
+
+
+
+var num = 12
+document.write("<br><br> The value of a++ is: " + a +"")
+document.write("<br>Now the value of a is "+ num +" ")
+
+
+document.write("<br><br>The value of --a is: " + a +"")
+document.write("<br>Now the vale of a is: " + a +"")
+
+var num1 = 10 
+document.write("<br><br> The value of a-- is "+ a +"")
+document.write("<br>Now the value of a is "+num1 +"")
+
+
+ 
