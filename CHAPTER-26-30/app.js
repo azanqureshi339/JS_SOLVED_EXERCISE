44f0efab25625ecb3a6cc02cc01f8764b33c87b9
Mohammad Azaan <azanqureshi339@gmail.com>
2026-07-10 21:49:29 +0500

    added chap:26-30 assignment

---
+++ CHAPTER-26-30/app.js
@@ -0,0 +1,62 @@
+// Question # 01
+
+// var userNumber = +prompt("Enter a Number?");
+// var roundOfNumber = Math.round(userNumber);
+// var floorValue = Math.floor(userNumber);
+// var cielValue = Math.ceil(userNumber);
+
+// document.write("User Number : " + userNumber + "<br>");
+// document.write("Round Off Value : " + roundOfNumber + "<br>");
+// document.write("Floor Value: " + floorValue + "<br>");
+// document.write("Ceil Value : " + cielValue);
+
+// Question # 02
+
+//  var userNumber = +prompt("Enter a Number?");
+//  var roundOfNumber = Math.round(userNumber);
+//  var floorValue = Math.floor(userNumber);
+//  var cielValue = Math.ceil(userNumber);
+
+//  document.write("User Number : " + -userNumber + "<br>");
+//  document.write("Round Off Value : " + -roundOfNumber + "<br>");
+//  document.write("Floor Value: " + -floorValue + "<br>");
+//  document.write("Ceil Value : " + -cielValue);
+
+// Question # 03
+
+// var num = -4;
+// document.write("The absolute Value of " + num + " is " + Math.abs(num));
+
+// Question # 04
+
+// var randomNumber = Math.ceil(Math.random() * 6);
+// document.write("Random dice value : " + randomNumber);
+
+// Question # 05
+
+// var flip = Math.ceil(Math.random() * 2);
+// if (flip === 1) {
+//     console.log("Head");
+// }else{
+//     console.log("Tail");
+// }
+
+// Question # 06
+
+// var randomNumber = Math.floor(Math.random() * 100);
+// document.write("Random Number Between 1 and 100" + " : " + randomNumber);
+
+// Quesiton # 07
+
+// var num = parseFloat(prompt("Enter a Number?"));
+// document.write("The Weight of user is : " + num);
+
+// Question # 08
+
+// var userInput = +prompt("Enter a Secret Number?")
+// var myNumber = 6;
+// if (userInput === myNumber) {
+//     alert("Congragulation!🎉");
+// } else{
+//     alert("Wrong Number...🥱Please try again🥴");
+// }
